35e86a98bed72de0faad33d5458dbe319950fa92
Mujeebhussain153 <mujeebhussain153@gmail.com>
2025-06-05 22:41:13 +0530

    main: added a function that parses text based on the type of the file (PDF or DOCX)

--- backend/utils/resume_parser.py
+++ backend/utils/resume_parser.py
@@ -21,3 +21,17 @@ def extract_text_from_docx(filename: str):
     except Exception as e:
         print(e)
         return
+
+# This Function is a passthrough function that decides which function to execute based on the file type provided
+def extract_resume_content(filepath: str):
+    try:
+        extension = os.path.splitext(filepath)
+        if extension == ".pdf":
+            return extract_text_from_pdf(filepath)
+        elif extension == ".docx":
+            return extract_text_from_docx(filepath)
+        else:
+            return "Unsupported file type provided"
+    except Exception as e:
+        print(e)
+        return
