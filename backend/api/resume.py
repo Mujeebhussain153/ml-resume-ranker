ed93802495532d3e666da9ecd031d6fd600ffbda
Mujeebhussain153 <mujeebhussain153@gmail.com>
2025-06-08 22:13:24 +0530

    main: added code for the resume_parser route and added code to preprocess the actual text using natural language tool kit

--- backend/api/resume.py
+++ backend/api/resume.py
@@ -7,7 +7,16 @@ router = APIRouter()
 @router.post("/parse-resume")
 async def parse_resume(file:UploadFile = File(...)):
     try:
-        pass
+        save_path = f"..\\temp\{file.filename}"
+        with open(save_path, "wb") as f:
+            f.write(await file.read())
+        raw_text = extract_resume_content(save_path)
+        cleaned_text = clean_text(raw_text)
+        return {
+            "filename": file.filename,
+            "raw_text": raw_text[:300],
+            "cleaned_text": cleaned_text[:300]
+        }
     except Exception as e:
         print(e)
         return
