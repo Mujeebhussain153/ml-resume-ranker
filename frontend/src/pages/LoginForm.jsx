a8676d4aec855ebb153d5198595364cb04c60e61
Mujeebhussain153 <mujeebhussain153@gmail.com>
2025-06-20 23:03:02 +0530

    main: Made minute changes in the Login Form

--- frontend/src/pages/LoginForm.jsx
+++ frontend/src/pages/LoginForm.jsx
@@ -2,11 +2,11 @@ const LoginForm = () => {
     return(
         <div className="border-2 max-w h-screen flex items-center justify-center bg-black">
             <div className="size-100 shadow-lg shadow-red-500 py-10">
-            <label for="username" className="text-white text-shadow-lg text-shadow-red-50">User Name: </label>
-            <input type="text" id="username" name="username" className="border-white"/><br />
-            <label for="password" className="text-white text-shadow-lg text-shadow-red-50">Password: </label>
-            <input type="text" id="password" name="password" /><br />
-            <button type="Submit" className="text-white text-shadow-lg text-shadow-red-50 border-white">Submit</button>
+                <label for="username" className="text-white text-shadow-lg text-shadow-red-50">User Name: </label><br />
+                <input type="text" id="username" name="username" className="border-white"/><br />
+                <label for="password" className="text-white text-shadow-lg text-shadow-red-50">Password: </label><br />
+                <input type="text" id="password" name="password" /><br />
+                <input type="button" className="text-white"/>
             </div>
         </div>
     )
